95d1b3967fd74fcf27dc1b3944c53495d0efd194
Decsu-k <k-rozhin@mail.ru>
2022-12-14 23:05:52 +0300

    CRUD

---
+++ EsoftRozhin/Pages/AddEditPageE.xaml.cs
@@ -0,0 +1,65 @@
+﻿using EsoftRozhin.AppDataFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Task = EsoftRozhin.AppDataFile.Task;
+
+namespace EsoftRozhin.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPageE.xaml
+    /// </summary>
+    public partial class AddEditPageE : Page
+    {
+        private Task _currentTask = new Task();
+        public AddEditPageE()
+        {
+            InitializeComponent();
+            DataContext = _currentTask;
+            ComboStatus.ItemsSource = EsoftBaseRozhinEntities.GetContext().StatusTask.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentTask.Title))
+                errors.AppendLine("Укажите название задания");
+
+            //if (errors.Length > 0)
+            //{
+            //    MessageBox.Show(errors.ToString());
+            //    return;
+            //}
+            if (_currentTask.ID == 0)
+                EsoftBaseRozhinEntities.GetContext().Task.Add(_currentTask);
+
+            try
+            {
+                EsoftBaseRozhinEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+    }
+}
--- EsoftRozhin/Pages/ExecutorPage.xaml.cs
+++ EsoftRozhin/Pages/ExecutorPage.xaml.cs
@@ -24,8 +24,31 @@ namespace EsoftRozhin.Pages
         public ExecutorPage()
         {
             InitializeComponent();
+            var allTypes = EsoftBaseRozhinEntities.GetContext().StatusTask.ToList();
+            allTypes.Insert(0, new StatusTask
+            {
+                Status = "Все типы"
+            });
+            ComboType.ItemsSource = allTypes;
+
+            ComboType.SelectedIndex = 0;
+
             var currentTask = EsoftBaseRozhinEntities.GetContext().Task.ToList();
             LViewExecutor.ItemsSource = currentTask;
+
+        }
+
+        private void UpdateExecutor()
+        {
+            var currentExecutor = EsoftBaseRozhinEntities.GetContext().StatusTask.ToList();
+
+            //if (ComboType.SelectedIndex > 0)
+            //    currentExecutor = currentExecutor.Where(p => p.Status.Contains(ComboType.SelectedItem as StatusTask)).ToList();
+
+            //currentExecutor = currentExecutor.Where(p => p.Status.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            //LViewExecutor.ItemsSource = currentExecutor.OrderBy(p => p.Status).ToList();
+
         }
 
         private void ButtonBack_Click(object sender, RoutedEventArgs e)
@@ -33,14 +56,38 @@ namespace EsoftRozhin.Pages
             Manager.MainFrame.GoBack();
         }
 
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateExecutor();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateExecutor();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPageE());
+        }
+
         private void BtnDelete_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void BtnEdit_Click_1(object sender, RoutedEventArgs e)
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
+            Manager.MainFrame.Navigate(new AddEditPageE());
+        }
 
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                EsoftBaseRozhinEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                //Task.ItemsSource = EsoftBaseRozhinEntities.GetContext().Task.ToList();
+            }
         }
     }
 }
